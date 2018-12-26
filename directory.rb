66d59c6445ab569d9c88e99f62c7a219ee02c3d4
Debora <debora.piu92@gmail.com>
2018-12-26 20:09:17 +0100

    code improved separating methods

--- directory.rb
+++ directory.rb
@@ -1,26 +1,37 @@
+@students = []
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students."
-    puts "2. Show the students."
-    puts "9. Exit."
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students(students)
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you mean. Try again."
-    end
+    print_menu
+    process(gets.chomp) # improved from: selection = gets.chomp \n process(selection)
   end
 end
 
-def input_students(students)
+def process(selection)
+  case selection
+    when "1"
+      @students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean. Try again."
+  end
+end
+
+def print_menu
+  puts "1. Input the students."
+  puts "2. Show the students."
+  puts "9. Exit."
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return twice"
   # get an input from the user
@@ -29,17 +40,17 @@ def input_students(students)
   while !name.empty? do
     puts "What's his/her cohort start?"
     cohort = gets.chomp
-    students << {name: name, cohort: cohort}
-    if students.count == 1
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
       puts "Now we have 1 student. Add more:"
     else
-      puts "Now we have #{students.count} students. Add more:"
+      puts "Now we have #{@students.count} students. Add more:"
     end
     # get another input to restart the loop
     name = gets.chomp
   end
   # return the array
-  students
+  @students
 end
 
 def print_header
@@ -47,21 +58,21 @@ def print_header
   puts "-------------".center(title.size)
 end
 
-def print(names)
-  if names.empty?
+def print_students_list
+  if @students.empty?
     puts "No students yet."
   else
-    names.each.with_index(1) do |name, index|
+    @students.each.with_index(1) do |name, index|
       puts "#{index}. #{name[:name]} (#{name[:cohort]} cohort)"
     end
   end
 end
 
-def print_footer(array)
-  if array.count == 1
+def print_footer
+  if @students.count == 1
     puts "Overall, we have 1 great student"
   else
-    puts "Overall, we have #{array.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
