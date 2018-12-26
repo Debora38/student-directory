17ad14055a838b5e13b49257d4d14e0afb50a5b5
Debora <debora.piu92@gmail.com>
2018-12-26 22:19:53 +0100

    students are saved to a file csv

--- directory.rb
+++ directory.rb
@@ -14,6 +14,8 @@ def process(selection)
       show_students
     when "9"
       exit
+    when "3"
+      save_students
     else
       puts "I don't know what you mean. Try again."
   end
@@ -22,6 +24,7 @@ end
 def print_menu
   puts "1. Input the students."
   puts "2. Show the students."
+  puts "3. Save the list to students.csv"
   puts "9. Exit."
 end
 
@@ -74,6 +77,16 @@ def print_footer
   else
     puts "Overall, we have #{@students.count} great students"
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
